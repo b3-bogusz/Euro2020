785c1f4decdb16d038019fdcd92c0104a3a988b9
b3-bogusz <b3.bogusz@gmail.com>
2021-06-17 13:32:34 +0200

    add request data method from api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,7 +8,7 @@ class SearchBar extends React.Component {
         event.preventDefault();
 
         // call callback from parent component
-
+        this.props.onSubmit(this.state.term);
     }
 
     render() {
@@ -16,7 +16,7 @@ class SearchBar extends React.Component {
             <div className="search-bar ui segment">
                 <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
-                        <label>Video search</label>
+                        <label>Game video search</label>
                         <input type="text"
                                value={this.state.term}
                                onChange={(e) => this.setState({ term: e.target.value})}
