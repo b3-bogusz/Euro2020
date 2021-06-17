339b797d02ee95d974814ad59fcb456cb3dfc11c
b3-bogusz <b3.bogusz@gmail.com>
2021-06-17 19:04:22 +0200

    add video player

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,7 +4,10 @@ import VideoItem from "./VideoItem";
 const VideoList = ({ videos, onVideoSelect }) => {
 
     const renderedList = videos.map((video) => {
-        return <VideoItem onVideoSelect={onVideoSelect} key={video.id} video={video}/>
+        return <VideoItem onVideoSelect={onVideoSelect}
+                          key={video.id.videoId}
+                          video={video}
+        />
     });
 
     return <div className="ui relaxed divided list">{renderedList}</div>
